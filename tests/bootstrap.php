35b91b6773731a04a8840b2fc1824615e7f227c2
charescape <53265646+charescape@users.noreply.github.com>
2024-10-04 16:51:51 +0800

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,7 +9,7 @@ $_SERVER['SCRIPT_NAME'] = '/' . __DIR__;
 $_SERVER['SCRIPT_FILENAME'] = __FILE__;
 
 require_once(__DIR__ . '/../vendor/autoload.php');
-require_once(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
+require_once(__DIR__ . '/../vendor/zivvio/zivvio/framework/Yii.php');
 
 Yii::setAlias('@yiiunit/twig', __DIR__);
 
